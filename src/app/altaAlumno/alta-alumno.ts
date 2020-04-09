7a56d1bcf186e5df1e2d03071de186c80cf3355b
Gabriela Alcantu <gaby.alcantu.90@live.com.ar>
2020-04-09 14:27:45 -0300

    Primer Commit

---
+++ src/app/altaAlumno/alta-alumno.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-altaAlumno',
+    templateUrl: './alta-alumno.component.html',
+    styleUrls: [ './alta-alumno.component.css' ]
+})
+
+export class altaAlumnoComponent{
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './prueba/prueba.component'
+import {altaAlumnoComponent} from './altaAlumno/alta-alumno'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent,
+    altaAlumnoComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
