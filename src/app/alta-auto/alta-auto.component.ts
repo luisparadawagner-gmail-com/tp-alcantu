27333b2757d2c44cf77efc06ef06420d50a36072
Gabriela Alcantu <gaby.alcantu.90@live.com.ar>
2020-04-25 21:15:33 -0300

    Se crea alta-auto como practica de formularios

---
+++ src/app/alta-auto/alta-auto.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormArray} from '@angular/forms';
+
+@Component({
+    selector: 'app-alta-auto',
+    templateUrl: './alta-auto.component.html',
+    styleUrls: [ './alta-auto.component.css' ]
+})
+
+export class AltaAutoComponent{
+
+    constructor(private fb: FormBuilder) {}
+
+    autoForm = this.fb.group({
+        marca: ['BMW', Validators.required],
+        modelo: [''],
+        version: [''],
+        origen: [''],
+        velocidadMax: this.fb.group({
+            kmh:[''],
+            mph:['']
+        }),
+        puertas: this.fb.array( [this.fb.control('') ]),
+        colores: this.fb.array( [this.fb.control('') ])
+    });
+
+    get puertas(){
+        return this.autoForm.get('puertas') as FormArray;
+    }
+
+    get colores(){
+        return this.autoForm.get('colores') as FormArray;
+    }
+
+    agregarPuerta(){
+        this.puertas.push(this.fb.control(''));
+    }
+
+    agregarColor(){
+        this.colores.push(this.fb.control(''));
+    }
+
+    submit(){
+        debugger;
+        this.autoForm.value;
+
+        this.autoForm.setValue({
+            marca:'Mercedes Benz',
+            modelo:'Clase C C200 Kompressor ',
+            version:'2001',
+            origen:'Alemania',
+            velocidadMax:{
+                kmh:'230',
+                mph:'143'
+            },
+            puertas:['3'],
+            colores:['Rojo']
+        })
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 import { AltaAlumnoComponent } from './alta-alumno/alta-alumno.component';
+import { AltaAutoComponent } from './alta-auto/alta-auto.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-	declarations: [ AppComponent, AltaAlumnoComponent],
-	imports: [ BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule ],
+	declarations: [ AppComponent, AltaAlumnoComponent, AltaAutoComponent],
+	imports: [ BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, MatInputModule, MatFormFieldModule, MatButtonModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
