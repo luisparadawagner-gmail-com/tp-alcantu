72d34997121f2cfa5add396b0d3949fadf93d610
LUIS ALBERTO PARADA <luisparadawagner@gmail.com>
2020-04-27 18:18:48 -0300

    Se corrige arrays en setValue

--- src/app/alta-auto/alta-auto.component.ts
+++ src/app/alta-auto/alta-auto.component.ts
@@ -1,60 +1,58 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators, FormArray} from '@angular/forms';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
-    selector: 'app-alta-auto',
-    templateUrl: './alta-auto.component.html',
-    styleUrls: [ './alta-auto.component.css' ]
+	selector: 'app-alta-auto',
+	templateUrl: './alta-auto.component.html',
+	styleUrls: [ './alta-auto.component.css' ]
 })
+export class AltaAutoComponent {
+	constructor(private fb: FormBuilder) {}
 
-export class AltaAutoComponent{
+	autoForm = this.fb.group({
+		marca: [ 'BMW', Validators.required ],
+		modelo: [ '' ],
+		version: [ '' ],
+		origen: [ '' ],
+		velocidadMax: this.fb.group({
+			kmh: [ '' ],
+			mph: [ '' ]
+		}),
+		puertas: this.fb.array([ this.fb.control('') ]),
+		colores: this.fb.array([ this.fb.control('') ])
+	});
 
-    constructor(private fb: FormBuilder) {}
+	get puertas() {
+		return this.autoForm.get('puertas') as FormArray;
+	}
 
-    autoForm = this.fb.group({
-        marca: ['BMW', Validators.required],
-        modelo: [''],
-        version: [''],
-        origen: [''],
-        velocidadMax: this.fb.group({
-            kmh:[''],
-            mph:['']
-        }),
-        puertas: this.fb.array( [this.fb.control('') ]),
-        colores: this.fb.array( [this.fb.control('') ])
-    });
+	get colores() {
+		return this.autoForm.get('colores') as FormArray;
+	}
 
-    get puertas(){
-        return this.autoForm.get('puertas') as FormArray;
-    }
+	agregarPuerta() {
+		this.puertas.push(this.fb.control(''));
+	}
 
-    get colores(){
-        return this.autoForm.get('colores') as FormArray;
-    }
+	agregarColor() {
+		this.colores.push(this.fb.control(''));
+	}
 
-    agregarPuerta(){
-        this.puertas.push(this.fb.control(''));
-    }
+	submit() {
+		debugger;
+		this.autoForm.value;
 
-    agregarColor(){
-        this.colores.push(this.fb.control(''));
-    }
-
-    submit(){
-        debugger;
-        this.autoForm.value;
-
-        this.autoForm.setValue({
-            marca:'Mercedes Benz',
-            modelo:'Clase C C200 Kompressor ',
-            version:'2001',
-            origen:'Alemania',
-            velocidadMax:{
-                kmh:'230',
-                mph:'143'
-            },
-            puertas:['3'],
-            colores:['Rojo']
-        })
-    }
+		this.autoForm.setValue({
+			marca: 'Mercedes Benz',
+			modelo: 'Clase C C200 Kompressor ',
+			version: '2001',
+			origen: 'Alemania',
+			velocidadMax: {
+				kmh: '230',
+				mph: '143'
+			},
+			puertas: this.autoForm.get('puertas').value,
+			colores: this.colores.value
+		});
+	}
 }
